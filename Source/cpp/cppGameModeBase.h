cc574dcc998af3a8048e186b9fe2fc8f5a92ae20
Testacc991 <70212365+Testacc991@users.noreply.github.com>
2023-06-08 23:03:09 +0300

    Initial commit

---
+++ Source/cpp/cppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "cppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CPP_API AcppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
